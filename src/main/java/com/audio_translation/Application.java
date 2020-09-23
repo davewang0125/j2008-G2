5fae48d3e7e50457858e7777928522e37efa8b2a
bofengding <d.flippeddd@gmail.com>
2020-09-22 23:12:11 -0400

    Add Backend folder

--- src/main/java/com/audio_translation/Application.java
+++ src/main/java/com/audio_translation/Application.java
@@ -1,13 +1,10 @@
 package com.audio_translation;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages= {"com.audio_translation"})
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.audio_translation.dao")
 public class Application {
 
